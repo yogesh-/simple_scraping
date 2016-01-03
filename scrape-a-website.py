e546fb731c0c39d1949d42a3c65081c4816ff23e
yogesh- <yogesh.gupta38@gmail.com>
2016-01-03 10:21:10 +0530

    few changes made

--- scrape-a-website.py
+++ scrape-a-website.py
@@ -13,32 +13,42 @@ links = soup.find_all("a")
 
 g_data = soup.find_all("div",{"class":"info"})
 
+list_info = []
+
 for item in g_data:
 	business_name=item.contents[0].find_all("a",{"class":"business-name"})[0].text
 	try:
-		print item.contents[1].find_all("a",{"itemprop":"streetAddress"})[0].text
+		streetAddress = item.contents[1].find_all("a",{"itemprop":"streetAddress"})[0].text
 	except:
 		pass
 	try:
-		print item.contents[1].find_all("span", {"itemprop":"addressLocality"})[0].text.replace(',','')
+		addressLocality = item.contents[1].find_all("span", {"itemprop":"addressLocality"})[0].text.replace(',','')
 	except:
 		pass
 	try:
-		print item.contents[1].find_all("span", {"itemprop":"addressRegion"})[0].text
+		addressRegion = item.contents[1].find_all("span", {"itemprop":"addressRegion"})[0].text
 	except: 
 		pass
 	try:
-		print item.contents[1].find_all("span", {"itemprop":"postalCode"})[0].text
+		postalCode = item.contents[1].find_all("span", {"itemprop":"postalCode"})[0].text
 	except: 
 		pass
-	try:
-		print item.contents[1].find_all("li", {"class":"primary"})[0].text
-	except:
-		pass
+	# try:
+	# 	print item.contents[1].find_all("li", {"class":"primary"})[0].text
+	# except:
+	# 	pass
+	y = business_name,addressRegion,addressLocality,postalCode
+list_info.append(y)
+print list_info
+resultFile = open("output.csv",'wb')
+writer = csv.writer(resultFile)
+for item in list_info:
+	writer.writerow([item])
 
-def exim ():
-	c = csv.writer(open('data.csv','wb'))
-	for i in g_data:
-		c.writerow(['business-name','streetAddress','addressLocality','addressRegion','postalCode'])
 
-exim()
+# def exim ():
+# 	c = csv.writer(open('data.csv','wb'))
+# 	# for item in g_data:
+# 	c.writerow(["business_name","streetAddress","addressLocality","addressRegion","postalCode"])
+# 	c.writerow([list_info])
+# # exim()
